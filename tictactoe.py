2ab769e011b31b704bce0840e658ac2f858fa343
katahanz1 <andr3s.aguirr3.o@gmail.com>
2023-10-29 21:03:36 -0600

    feat: added window

---
+++ tictactoe.py
@@ -0,0 +1,25 @@
+from tkinter import *
+import random
+
+
+
+window = Tk()
+
+window.title("Tic-Tac-Toe")
+players = ["X", "O"]
+player = random.choice(players)
+board = [[0,0,0],
+         [0,0,0],
+         [0,0,0]]
+
+label = Label(text= player + " turn", font=("consolas", 40 ))
+label.pack(side="top")
+
+reset_button = Button(text = "restart", font=("consolas", 20))
+reset_button.pack(side="top")
+
+frame = Frame(window)
+frame.pack()
+
+window.mainloop()
+
